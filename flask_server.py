0feac1fe9b72790ce777276888ad62b3f61d0403
Francesco Maccari <franci.macca@gmail.com>
2019-12-09 23:52:31 +0100

    improved code

--- flask_server.py
+++ flask_server.py
@@ -25,11 +25,11 @@ def get_settings():
     with open("settings.json", "r") as settings_json:
         return json.load(settings_json)
 
-def create_markup(base64_data, media_type, username = None):
+def create_markup(base64_data, media_type = None, username = None):
     if username != None:
         return Markup((f"<hr><div class=\"username-text\">{username}</div><br>"))
     if media_type in ["video/mp4", "img/png"]:
-        content_tag = "img" if "img" in media_type else "video controls"
+        content_tag = "img" if media_type == "img/png" else "video controls"
         return Markup(f"<{content_tag} src=\"data:{media_type};base64,{base64_data}\" class=\"rendered-stories\"></{content_tag}>")
 
 def render_base64_media(base64_media):
@@ -37,15 +37,14 @@ def render_base64_media(base64_media):
     last_username = None
     for base64_data, media_type, username in base64_media:
         if username != last_username:
-            rendered_base64_media.append(create_markup(base64_data, None, username))
+            rendered_base64_media.append(create_markup(base64_data, username = username))
             last_username = username
         rendered_base64_media.append(create_markup(base64_data, media_type))
     return rendered_base64_media
 
 def get_rendered_folders(path, url):
     rendered_folders = []
-    if not os.path.exists(path):
-        return []
+    if not os.path.exists(path) : return []
     for folder in os.listdir(path):
         rendered_folders.append(Markup(f"<a href=\"{url}{folder}\" class=\"urls\">{folder}</a>"))
     return rendered_folders
@@ -53,11 +52,11 @@ def get_rendered_folders(path, url):
 def get_rendered_media(path):
     rendered_media = []
     for media in os.listdir(path):
-        if not media.endswith(".json"):
-            media_type = "img/png" if media.endswith(".jpg") else "video/mp4"
-            with open(f"{path}\\{media}", "rb") as media_element:
-                base64_media = base64.b64encode(media_element.read()).decode("utf-8")
-            rendered_media.append(create_markup(base64_media, media_type))
+        if  media.endswith(".json"): continue
+        media_type = "img/png" if media.endswith(".jpg") else "video/mp4"
+        with open(f"{path}\\{media}", "rb") as media_element:
+            base64_media = base64.b64encode(media_element.read()).decode("utf-8")
+        rendered_media.append(create_markup(base64_media, media_type))
     return rendered_media
 
 ################### ROUTES ###################
@@ -81,34 +80,30 @@ def index():
 @app.route("/settings/", methods=['GET','POST'])
 def settings():
     settings = get_settings()    # Gets the settings
-    cookie_path = settings["cookie_path"] if "cookie_path" in settings else "token.txt"
-    folder_path = settings["folder_path"]  if "folder_path" in settings else "ig_media"
-    updated_settings = {}
     if request.method == "POST":
+        updated_settings = {}
         for setting in request.form:
             if len(request.form[setting]) > 0:
                 updated_settings[setting] = request.form[setting]
         save_settings(updated_settings)
-    return render_template("settings.html", folder_path = updated_settings["folder_path"] if "folder_path" in updated_settings else folder_path, cookie_path = updated_settings["cookie_path"] if "cookie_path" in updated_settings else cookie_path)
+    cookie_path = settings["cookie_path"] if "cookie_path" in settings else "token.txt"
+    folder_path = settings["folder_path"]  if "folder_path" in settings else "ig_media"
+    return render_template("settings.html", folder_path = folder_path, cookie_path = cookie_path)
 
-@app.route("/gallery/", methods=['GET'])
-@app.route("/gallery/<username>", methods=['GET'])
-@app.route("/gallery/<username>/<date>", methods=['GET'])
-def gallery(username = None, date = None):
+@app.route("/gallery/", methods=['GET'], defaults = {"username": None, "date": None})
+@app.route("/gallery/<username>/", methods=['GET'], defaults = {"date": None})
+@app.route("/gallery/<username>/<date>/", methods=['GET'])
+def gallery(username, date):
     settings = get_settings()
     folder_path = settings["folder_path"]  if "folder_path" in settings else "ig_media" 
-    if username == None and date == None:
-        url = request.url
-        rendered_items = get_rendered_folders(folder_path, url)
-    elif date == None and username != None:
-        user_path = os.path.join(folder_path, username)
-        url = request.url
-        if not url.endswith("/") : url = url + "/"
-        rendered_items = get_rendered_folders(user_path, url)
-    elif date != None and username != None:
+    if date != None:
         date_path = os.path.join(os.path.join(folder_path, username), date)
         rendered_items = get_rendered_media(date_path)
-
+    elif username != None:
+        user_path = os.path.join(folder_path, username)
+        rendered_items = get_rendered_folders(user_path, request.url)
+    else:
+        rendered_items = get_rendered_folders(folder_path, request.url)
      
     return render_template("gallery.html", rendered_items = rendered_items )
 
