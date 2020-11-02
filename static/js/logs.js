19de9c75006b49df7a9b041c7fb5d51acf7b2c2c
Francesco Maccari <franci.macca@gmail.com>
2020-11-02 17:57:30 +0100

    Changed variables names, linted code

--- static/js/logs.js
+++ static/js/logs.js
@@ -1,23 +1,23 @@
 const API_PREFIX = 'api';
 const baseUrl = window.location.origin;
 
-const renderLogs = (logs) => {
-    const logsNode = document.getElementById('logs');
-    const root = document.createElement('div')
-    logs.forEach( (el) => {
-        const pNode = document.createElement('p');
-        pNode.innerText = el
-        root.appendChild(pNode)
-    })
-    logsNode.appendChild(root)
-}
+const renderLogs = (logLines) => {
+  const logsNode = document.getElementById('logs');
+  const root = document.createElement('div');
+  logLines.forEach((line) => {
+    const pNode = document.createElement('p');
+    pNode.innerText = line;
+    root.appendChild(pNode);
+  });
+  logsNode.appendChild(root);
+};
 
-const getLogs = async () => {
-    const requestUrl = `${baseUrl}/${API_PREFIX}/logs`;
-    const responseData = await (await fetch(requestUrl)).json();
-    renderLogs(responseData['logs']);
-}
+const getAndRenderLogs = async () => {
+  const requestUrl = `${baseUrl}/${API_PREFIX}/logs`;
+  const responseData = await (await fetch(requestUrl)).json();
+  renderLogs(responseData['logs']);
+};
 
 window.onload = () => {
-    getLogs();
+  getAndRenderLogs();
 };
